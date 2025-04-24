6e2915935c81234910a64b5cb426b50ced6afc46
ssussu <ssussu00623@gmail.com>
2025-04-24 14:06:54 +0900

    ==

--- repository/db.js
+++ repository/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'ssussu-db.cpgwkwuecf57.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'mysql1234',
+    user: 'admin',
+    password: 'ssussu1234',
     database: 'kurlydb',
 });
 
