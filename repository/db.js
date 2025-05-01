2be0458cba5c39c3d81ddfb721017182bae73b5a
ssussu <ssussu00623@gmail.com>
2025-05-01 12:09:44 +0900

    ==edit

--- repository/db.js
+++ repository/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'shoppy-redux-db.c5c00c6s66l9.ap-northeast-2.rds.amazonaws.com',
+    host: 'ssussu-db.cpgwkwuecf57.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
     user: 'admin',
-    password: 'Hwangsu94!',
+    password: 'ssussu1234',
     database: 'hrdb2019',
 });
 
